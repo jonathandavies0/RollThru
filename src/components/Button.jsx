c05eea909943db686349ed20d82c1e2df3eaefa3
Avni Girish <avni123.girish@gmail.com>
2023-07-11 08:15:01 -0400

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,9 @@ import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import "./ButtonStyle.css"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function BasicPopover() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -17,6 +20,17 @@ export default function BasicPopover() {
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
+
+  const time = [
+    {
+      value: 'am',
+      label: 'AM',
+    },
+    {
+      value: 'pm',
+      label: 'PM',
+    },
+  ];
 
   return (
     <div>
@@ -36,12 +50,35 @@ export default function BasicPopover() {
         <Typography sx={{ p: 2 }}>
             <div id="PopOver">
                 <h1>Skate Sesh Details!</h1>
-                <h3>Time: tbd </h3>
+                <h3>Time: 
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        >
+                        <TextField id="outlined-basic" label="Hour" variant="outlined" />
+                        <TextField
+                        select
+                        label="AM/PM"
+                        defaultValue="pm"
+                        >
+                        {time.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                            </MenuItem>
+                        ))}
+                        </TextField>
+                    </Box>
+                </h3>
                 <h3>Location: tbd </h3>
                 <p>Hey guys! Would you guys want to be in a skate sesh with me? Please come down to the Skate Park at the location and time I requested!!</p>
             </div>
         </Typography>
       </Popover>
+      
     </div>
   );
 }
